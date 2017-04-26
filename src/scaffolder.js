c1bdf569cda99851b4a0f7952c9988f770c1d75e
Mateusz Luczak <mateusz.luczak@cognifide.com>
2017-04-26 17:07:50 +0200

    New command

--- src/scaffolder.js
+++ src/scaffolder.js
@@ -1,5 +1,25 @@
-const scaffold = (name, src, dest) => {
+const path = require('path');
+const Metalsmith = require('metalsmith');
+const Handlebars = require('handlebars');
 
+const scaffold = (name, src, dest) => {
+  var metalsmith = Metalsmith(path.join(src, 'template'));
+  var data = Object.assign(metalsmith.metadata(), {
+    destDirName: name,
+    inPlace: dest === process.cwd(),
+    noEscape: true
+  });
+
+  console.log(name, src, dest);
+
+  metalsmith.clean(false)
+    .source('.') // start from template root instead of `./src` which is Metalsmith's default for `source`
+    .destination(dest)
+    .build(function (err, files) {
+      console.log(files);
+    })
+
+  return data
 };
 
 module.exports = scaffold;
